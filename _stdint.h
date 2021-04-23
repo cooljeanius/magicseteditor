77ef78357151b0689a75bf15460b23bbdf83c561
Eric Gallager <egall@gwmail.gwu.edu>
2021-04-22 21:15:28 -0400

    update autotools stuff some more

--- _stdint.h
+++ _stdint.h
@@ -2,7 +2,7 @@
 #define _MAGICSETEDITOR__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "magicseteditor 0.3.9"
-/* generated using gnu compiler gcc (MacPorts gcc48 4.8.1_3+universal) 4.8.1 */
+/* generated using gnu compiler Apple clang version 12.0.0 (clang-1200.0.32.29) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
